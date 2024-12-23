eb12989f1adb95b161426088824331d2db1ec945
Saurav Ganguly <work@sauravganguly.in>
2024-12-23 23:26:58 +0530

    react-native-paper-dropdown issue https://github.com/fateh999/react-native-paper-dropdown/issues/115

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,10 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    env: {
-      production: {
-        plugins: ["react-native-paper/babel"],
-      },
-    },
+    // env: {
+    //   production: {
+    //     plugins: ["react-native-paper/babel"],
+    //   },
+    // },
   };
 };
